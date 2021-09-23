3abd971ef0b187711b774431c04926d2bcec4ae7
Kumar Shantanu <kumar1.shantanu@hotmail.com>
2021-09-23 17:46:31 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -4,22 +4,18 @@ module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
       reactStrictMode: true,
-      env: {
-        mongodb_username: "admin",
-        mongodb_password: "admin123",
-        mongodb_clustername: "nextjsdata",
-        mongodb_database: "myBlogSite-Dev",
-      },
     };
   }
 
   return {
     reactStrictMode: true,
-    env: {
+  };
+};
+/**
+ * env: {
       mongodb_username: "admin",
-      mongodb_password: "admin123",
+      mongodb_password: "adminabc",
       mongodb_clustername: "nextjsdata",
       mongodb_database: "myBlogSite-Prod",
     },
-  };
-};
+ */
